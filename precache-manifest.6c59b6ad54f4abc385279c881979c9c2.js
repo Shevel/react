e79137063b63d5496789df7b4bc6648721f18afe
Vladislav Shevel <vladislav.shevel@gmail.com>
2020-10-16 19:47:25 +0300

    Updates

---
+++ precache-manifest.6c59b6ad54f4abc385279c881979c9c2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f35e0aeaef7e794f21b69709395ff1b",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "7afca05dfac3c76a87fe",
+    "url": "/react/static/css/3.027baebb.chunk.css"
+  },
+  {
+    "revision": "db71f32b2b80ca55b4eb",
+    "url": "/react/static/css/4.8eed2b78.chunk.css"
+  },
+  {
+    "revision": "d7acb40b8b4fed448d23",
+    "url": "/react/static/css/main.3db7a635.chunk.css"
+  },
+  {
+    "revision": "e488fbd588dc25f583c5",
+    "url": "/react/static/js/2.c2db7f35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.c2db7f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afca05dfac3c76a87fe",
+    "url": "/react/static/js/3.04e91a2b.chunk.js"
+  },
+  {
+    "revision": "db71f32b2b80ca55b4eb",
+    "url": "/react/static/js/4.f7e90d9e.chunk.js"
+  },
+  {
+    "revision": "d7acb40b8b4fed448d23",
+    "url": "/react/static/js/main.4da11f03.chunk.js"
+  },
+  {
+    "revision": "960b9130a52a2aa8ed30",
+    "url": "/react/static/js/runtime-main.f152175b.js"
+  },
+  {
+    "revision": "4217e8eebe63d10fea99aa6429489426",
+    "url": "/react/static/media/Spin.4217e8ee.svg"
+  },
+  {
+    "revision": "b4663671a25121f73975b3f999d5a0b9",
+    "url": "/react/static/media/logo.b4663671.png"
+  },
+  {
+    "revision": "88962eb747475dc855a307e6c283fc67",
+    "url": "/react/static/media/noavatar.88962eb7.jpg"
+  }
+]);
